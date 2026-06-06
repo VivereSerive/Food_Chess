67623fcadaa921a3e811fb5588da205e2c75e4a9
Sean Rivera <vivereserive@gmail.com>
2026-06-06 14:43:39 +1200

    Implemented Chess Piece Display
    Unable to figure out margins

--- scripts/chessBoard/chessBoard.gd
+++ scripts/chessBoard/chessBoard.gd
@@ -1,11 +1,17 @@
 class_name chessBoard
-extends Sprite2D
+extends Node2D
 ### Chess Board Script
 
 ## Variable Declerations
 
-# Board Stats
+# Board Config
 const boardSize : int = 8
+const bTopMargin : int = 20 # must match px height of the empty space.
+const bBottomMargin : int = 27 # must match px height of the empty space.
+const bLeftMargin : int = 7 # must match px width of the empty space.
+const bRightMargin : int = 7 # must match px width of the empty space.
+const squareWidth : int = 16 # must match px width of the square.
+const squareHeight : int = 12 # must match px height of the square
 var boardIndex : Array
 
 # Player
@@ -14,12 +20,16 @@ var playerState : bool # False = Selecting Move, True = Confirmed Move
 
 # Pieces
 var pieceMove : Array = [] # All possible moves of the selected chess piece
-var piecePos : Vector2
+var piecePos : Vector2 # References the position of the chess piece
 
-# Node References
+
+## Node References
 @onready var pieces: Node2D = $pieces
 @onready var dots: Node2D = $dots
 @onready var turn: Sprite2D = $turn
+
+# Texture Holder
+const PIECE_HOLDER = preload("uid://djgv2sh8uvyqb")
 
 # Black
 const B_BISHOP = preload("uid://b6sa1ussifsxs")
@@ -37,7 +47,57 @@ const W_PAWN = preload("uid://cwjoiqmxhdbok")
 const W_QUEEN = preload("uid://bpubkhoqy2mu6")
 const W_ROOK = preload("uid://dfpw1ef0y54hp")
 
-## Main Loop
+## Setup
+func _ready() -> void:
+	boardIndex.append([4, 2, 3, 5, 6, 3, 2, 4])
+	boardIndex.append([1, 1, 1, 1, 1, 1, 1, 1])
+	boardIndex.append([0, 0, 0, 0, 0, 0, 0, 0])
+	boardIndex.append([0, 0, 0, 0, 0, 0, 0, 0])
+	boardIndex.append([0, 0, 0, 0, 0, 0, 0, 0])
+	boardIndex.append([0, 0, 0, 0, 0, 0, 0, 0])
+	boardIndex.append([-1, -1, -1, -1, -1, -1, -1, -1])
+	boardIndex.append([-4, -2, -3, -5, -6, -3, -2, -4])
+	
+	_display_board()
 
+## Main Loop
+func _physics_process(delta: float) -> void:
+	pass
 
 ## Function Declerations
+func _display_board():
+	for boardRow in boardSize:
+		for boardColumn in boardSize:
+			## Initializing Pieces
+			var pieceInit = PIECE_HOLDER.instantiate()
+			pieces.add_child(pieceInit)
+			pieceInit.position = Vector2(boardColumn * squareWidth, -boardRow * squareHeight)
+			
+			# Assign Texture
+			# Positive = White, Negative = Black, ZERO = empty
+			# {"Pawn": 1, "Knight": 2, "Bishop": 3, "Rook": 4, "Queen": 5, "King": 6}
+			match boardIndex[boardRow][boardColumn]:
+				-6: pieceInit.texture = B_KING
+				-5: pieceInit.texture = B_QUEEN
+				-4: pieceInit.texture = B_ROOK
+				-3: pieceInit.texture = B_BISHOP
+				-2: pieceInit.texture = B_KNIGHT
+				-1: pieceInit.texture = B_PAWN
+				0: pieceInit.texture = null
+				6: pieceInit.texture = W_KING
+				5: pieceInit.texture = W_QUEEN
+				4: pieceInit.texture = W_ROOK
+				3: pieceInit.texture = W_BISHOP
+				2: pieceInit.texture = W_KNIGHT
+				1: pieceInit.texture = W_PAWN
+			
+			# Assign z-index
+			match boardRow:
+				0: pieceInit.z_index = 8
+				1: pieceInit.z_index = 7
+				2: pieceInit.z_index = 6
+				3: pieceInit.z_index = 5
+				4: pieceInit.z_index = 4
+				5: pieceInit.z_index = 3
+				6: pieceInit.z_index = 2
+				7: pieceInit.z_index = 1
