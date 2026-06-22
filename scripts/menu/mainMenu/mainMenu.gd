cd728ab17f0f7d22f1ac198809b0cc2c9543f169
Sean Rivera <vivereserive@gmail.com>
2026-06-23 10:21:20 +1200

    Updated mainMenu

--- scripts/menu/mainMenu/mainMenu.gd
+++ scripts/menu/mainMenu/mainMenu.gd
@@ -1,11 +1,16 @@
+class_name MainMenu
 extends Control
 
+## Paramaters
+# Scene References
+const SETTINGS_MENU = preload("uid://cuu3snvd8mph")
+const CHESS_MAIN = preload("uid://mhdy3qanvcfa")
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _on_btn_start_pressed() -> void:
+	get_tree().change_scene_to_packed(CHESS_MAIN)
 
+func _on_btn_settings_pressed() -> void:
+	get_tree().change_scene_to_packed(SETTINGS_MENU)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_btn_exit_pressed() -> void:
+	get_tree().quit()
